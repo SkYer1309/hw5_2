caa51295e6be4aab3849e9471a7120b5a3c25aa6
skyer <sky13net84@gmail.com>
2026-02-11 11:59:50 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 
 public class Main {
     public static void main(String[] args) {
-
-            System.out.println(" Задание 1\n");
+        byte clientOS = 0;
+        System.out.println("\n Задание 1");
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке:\n");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android  по ссылке:");
         }
+        System.out.println(" \nЗадание 2");
     }
 }
